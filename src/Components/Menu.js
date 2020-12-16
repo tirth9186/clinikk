39a36d1d3dc72d340aafb3a9ea9e1853c947d958
tirth9186 <tirthchaudhary9960@gmail.com>
2020-12-16 16:06:42 +0530

    1 binaries uploaded

---
+++ src/Components/Menu.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const logoStyle = {
+'font-size':'6vw',
+'font-family': 'Baloo Bhai',
+'font-style': 'normal',
+'font-weight': 'normal',
+'color': '#FFFFFF'
+}
+
+
+export default function Menu(){
+  return(
+    <div className="col bg-dark">
+      <p style={logoStyle}>F.</p>
+    </div>
+  );
+}

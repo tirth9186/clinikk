9b17838d9533e0c3da7c306e8788950a1d41a401
tirth9186 <tirthchaudhary9960@gmail.com>
2020-12-17 18:32:42 +0530

    1 file modified, 1 added, 1 deleted

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -3,16 +3,18 @@ import SearchField from "react-search-field";
 import NotificationBadge from 'react-notification-badge';
 import {Effect} from 'react-notification-badge';
 import { BsBell } from "react-icons/bs";
+import image from "./image 13.png";
 export default function Navigation(){
+  const ct=0;
   return(
     <div className="d-flex mt-1">
         <SearchField classNames="mr-3" />
         <BsBell size="30px" ></BsBell>
         <span>
-        <NotificationBadge count='1' effect={Effect.SCALE}/>  
+        <NotificationBadge count={ct} effect={Effect.SCALE}/>  
         </span>
         <span>
-        
+          <img src ={image}/>
         </span>
     </div>
   );

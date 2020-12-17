13ca4a0b62fd72440a70efad998909f6d79ab2fd
tirth9186 <tirthchaudhary9960@gmail.com>
2020-12-17 18:09:38 +0530

    1 binaries uploaded

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import SearchField from "react-search-field";
- 
-
+import NotificationBadge from 'react-notification-badge';
+import {Effect} from 'react-notification-badge';
+import Notification from '../../public/images/notification.jpg'
 export default function Navigation(){
   return(
     <div className="d-flex">
         <SearchField />
-        
+          <img src = {Notification}/>
+        <div>
+          <NotificationBadge count='2' effect={Effect.SCALE}/>
+        </div>        
     </div>
   );
 }
